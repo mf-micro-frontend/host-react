2daa2fa8996e9d29144586fa6954d9300b6655f2
BajicMladen <bajicmladen33@gmail.com>
2025-01-27 23:16:50 +0100

    Make adding to cart action easier.

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -8,6 +8,22 @@ export const GlobalProvider = ({ children }) => {
   const [selectedBook, setSelectedBook] = useState(null);
   const [searchText, setSearchText] = useState("");
 
+  const addToCart = (data) => {
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.bookId === data.bookId);
+
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.bookId === data.bookId
+            ? { ...item, quantity: item.quantity + data.quantity }
+            : item
+        );
+      }
+
+      return [...prevCart, { ...data, quantity: data.quantity }];
+    });
+  };
+
   return (
     <GlobalContext.Provider
       value={{
@@ -17,6 +33,7 @@ export const GlobalProvider = ({ children }) => {
         setSelectedBook,
         searchText,
         setSearchText,
+        addToCart,
       }}
     >
       {children}
